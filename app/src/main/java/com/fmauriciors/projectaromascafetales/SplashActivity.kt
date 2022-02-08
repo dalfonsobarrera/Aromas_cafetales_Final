debbb16f86243a02b3190b442e32aefd32ac11d1
Diego Barrera Barrios <diego.barrera@udea.edu.co>
2022-02-08 18:35:24 -0500

    lla

--- app/src/main/java/com/fmauriciors/projectaromascafetales/SplashActivity.kt
+++ app/src/main/java/com/fmauriciors/projectaromascafetales/SplashActivity.kt
@@ -1,11 +1,34 @@
 package com.fmauriciors.projectaromascafetales
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fmauriciors.projectaromascafetales.databinding.ActivitySplashBinding
+import java.util.*
+import kotlin.concurrent.timerTask
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var splashBinding: ActivitySplashBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        splashBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(splashBinding.root)
+
+        val timer = Timer()
+        timer.schedule(
+            timerTask {
+                goToLoginActivity()
+            }, 2000
+        )
+
+    }
+
+    private fun goToLoginActivity() {
+        val intent = Intent(this, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or  Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+
     }
 }
