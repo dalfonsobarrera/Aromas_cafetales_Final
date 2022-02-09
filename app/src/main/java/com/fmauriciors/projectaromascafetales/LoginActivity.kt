2f25c2cad7aa0aecbcdb49dc4ea924d0b97caf26
Diego Barrera Barrios <diego.barrera@udea.edu.co>
2022-02-08 19:41:00 -0500

    ff

--- app/src/main/java/com/fmauriciors/projectaromascafetales/LoginActivity.kt
+++ app/src/main/java/com/fmauriciors/projectaromascafetales/LoginActivity.kt
@@ -1,11 +1,25 @@
 package com.fmauriciors.projectaromascafetales
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fmauriciors.projectaromascafetales.databinding.ActivityLoginBinding
+
 
 class LoginActivity : AppCompatActivity() {
+
+    private  lateinit var loginBinding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        loginBinding = ActivityLoginBinding.inflate(layoutInflater)
+
+        loginBinding.registerTextView.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        setContentView(loginBinding.root)
     }
 }
