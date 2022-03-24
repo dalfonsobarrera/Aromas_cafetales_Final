d1c1adaac808ac22e70f59e329058e717f8f63f6
Diego Barrera Barrios <diego.barrera@udea.edu.co>
2022-03-23 21:43:10 -0500

    c

--- app/src/main/java/com/fmauriciors/projectaromascafetales/ui/main/MainActivity.kt
+++ app/src/main/java/com/fmauriciors/projectaromascafetales/ui/main/MainActivity.kt
@@ -4,15 +4,26 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ActivityNavigator
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.fmauriciors.projectaromascafetales.R
+import com.fmauriciors.projectaromascafetales.databinding.ActivityBottomBinding
 import com.fmauriciors.projectaromascafetales.databinding.ActivityMainBinding
 import com.fmauriciors.projectaromascafetales.ui.loginuser.LoginUserFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mainBinding: ActivityMainBinding
+    private lateinit var bottomBarbinding: ActivityBottomBinding
 
     override fun onBackPressed(){
         super.onBackPressed()
@@ -21,23 +32,32 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mainBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(mainBinding.root)
 
-        var emailReceived : String?= ""
+      bottomBarbinding = ActivityBottomBinding.inflate(layoutInflater)
+        setContentView(bottomBarbinding.root)
 
-        val credenciales = intent.extras
-        //hello world
-        if (credenciales != null) {
-            //credenciales.getString("name")
-            //credenciales.getString("telephone")
-            //credenciales.getString("brand")
-            emailReceived = credenciales.getString("email")        //se toman los valores de las variables en la actividad registro
+        val navView: BottomNavigationView = bottomBarbinding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_bottom)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.listProductsFragment, R.id.favoriteFragment, R.id.shopingFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if(destination.id == R.id.splashAcFragment
+                || destination.id == R.id.loginUserFragment
+                || destination.id == R.id.registerUserFragment) {
+                navView.visibility = View.GONE
+                supportActionBar?.hide()
+            } else {
+                navView.visibility = View.VISIBLE
+                supportActionBar?.show()
+            }
         }
-
-        //val emailMain : TextView = findViewById(R.id.email_main)
-        //emailMain.text = emailReceived
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -57,4 +77,8 @@ class MainActivity : AppCompatActivity() {
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or  Intent.FLAG_ACTIVITY_CLEAR_TASK
         startActivity(intent)
     }
+
+
+
+
 }
