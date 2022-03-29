7b21aea5fc9313abc0cc0d9d60bd388564d29cfd
Diego Barrera Barrios <diego.barrera@udea.edu.co>
2022-03-28 19:56:51 -0500

    c

--- app/src/main/java/com/fmauriciors/projectaromascafetales/ui/main/MainActivity.kt
+++ app/src/main/java/com/fmauriciors/projectaromascafetales/ui/main/MainActivity.kt
@@ -1,52 +1,47 @@
 package com.fmauriciors.projectaromascafetales.ui.main
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.ActivityNavigator
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.fmauriciors.projectaromascafetales.R
-import com.fmauriciors.projectaromascafetales.databinding.ActivityBottomBinding
 import com.fmauriciors.projectaromascafetales.databinding.ActivityMainBinding
-import com.fmauriciors.projectaromascafetales.databinding.FragmentRegisterUserBinding
-import com.fmauriciors.projectaromascafetales.ui.loginuser.LoginUserFragment
-import com.fmauriciors.projectaromascafetales.ui.registeruser.RegisterUserViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mainBinding: ActivityMainBinding
-    private lateinit var bottomBarbinding: ActivityBottomBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var navController: NavController
 
-
-
-    override fun onBackPressed(){
+    override fun onBackPressed() {
         super.onBackPressed()
-        Toast.makeText(this,"Cerrado", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Cerrado", Toast.LENGTH_SHORT).show()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-      bottomBarbinding = ActivityBottomBinding.inflate(layoutInflater)
-        setContentView(bottomBarbinding.root)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
         auth = Firebase.auth
 
-        val navView: BottomNavigationView = bottomBarbinding.navView
-        val navController = findNavController(R.id.nav_host_fragment_activity_bottom)
+        val navView: BottomNavigationView = mainBinding.navView
+        // val navController = findNavController(R.id.nav_host_fragment_activity_bottom)
+        val navHostFragment: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_activity_bottom) as NavHostFragment
+
+        navController = navHostFragment.navController
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.listProductsFragment, R.id.favoriteFragment, R.id.shopingFragment
@@ -56,9 +51,10 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
         navController.addOnDestinationChangedListener { _, destination, _ ->
-            if(destination.id == R.id.splashAcFragment
+            if (destination.id == R.id.splashAcFragment
                 || destination.id == R.id.loginUserFragment
-                || destination.id == R.id.registerUserFragment) {
+                || destination.id == R.id.registerUserFragment
+            ) {
                 navView.visibility = View.GONE
                 supportActionBar?.hide()
             } else {
@@ -66,7 +62,7 @@ class MainActivity : AppCompatActivity() {
                 supportActionBar?.show()
             }
         }
-         }
+    }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.overflow_menu, menu)
@@ -74,25 +70,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId){
+        super.onOptionsItemSelected(item)
+        auth.signOut()
+        navController.navigate(item.itemId)
+        return true
 
-            R.id.menu_sing_out -> {
-                auth.signOut()
-                goToLogin()
-            }
-        }
-        return super.onOptionsItemSelected(item)
     }
 
     private fun goToLogin() {
-        findNavController(R.id.nav_host_fragment_activity_bottom)
+        // findNavController.navigate(R.id.nav_host_fragment_activity_bottom)
         /*val intent = Intent(this, LoginUserFragment::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or  Intent.FLAG_ACTIVITY_CLEAR_TASK
         startActivity(intent)*/
     }
-
-
-
 
 
 }
